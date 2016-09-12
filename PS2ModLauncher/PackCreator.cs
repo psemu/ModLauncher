300f2f6b18a83ecc05637bc7e300adfc162715d9
Jhett Black <jhettblack@hotmail.com>
2016-09-12 20:57:04 +1000

    Implement chunking of pack files.
    Fixes issue where packing more that 255 files would result in truncating.

--- PS2ModLauncher/PackCreator.cs
+++ PS2ModLauncher/PackCreator.cs
@@ -29,8 +29,11 @@ namespace PS2ModLauncher
         }
     }
 
-    class ChunkHeader
+    class Chunk
     {
+        public List<FileHeader> fheader = new List<FileHeader>();
+        public List<byte[]> fileData = new List<byte[]>();
+
         public uint NextChunkOffset;
         public uint FileCount;
         public FileHeader[] files;
@@ -40,72 +43,122 @@ namespace PS2ModLauncher
             EndianBinaryWriter wr = new EndianBinaryWriter(EndianBitConverter.Big, new MemoryStream());
             wr.Write(NextChunkOffset);
             wr.Write(FileCount);
+
             foreach (FileHeader h in files)
             {
                 wr.Write(h.Encode());
             }
 
-            return (wr.BaseStream as MemoryStream).ToArray();
+            return ((MemoryStream)wr.BaseStream).ToArray();
         }
     }
 
 
     class PackCreator
     {
-      
+
         public static void CreatePackFromFiles(string[] files, string savePath)
         {
-            int fileCount = files.Length;
+            List<Chunk> chunks = new List<Chunk>();
 
-            List<FileHeader> fheader = new List<FileHeader>();
-            List<byte[]> fileData = new List<byte[]>();
+            Chunk chunk = new Chunk();
+
+            uint chunkFilesProcessed = 0;
 
             //Create File Headers for each file to pack
-            foreach (string file in files)
+            for (int i = 0; i < files.Length; i++)
             {
+                string file = files[i];
                 byte[] fdata = File.ReadAllBytes(file);
-                fileData.Add(fdata);
+                chunk.fileData.Add(fdata);
+
                 Crc32 crc = new Crc32();
                 byte[] c = crc.ComputeHash(fdata);
+
                 string filename = file.Substring(file.LastIndexOf("\\") + 1);
-                FileHeader h = new FileHeader()
+
+                FileHeader h = new FileHeader
                 {
                     name_len = (uint)filename.Length,
                     name = Encoding.ASCII.GetBytes(filename),
                     offset = 0,
                     length = (uint)fdata.Length,
-                    crc32 = (uint)BitConverter.ToInt32(c, 0),
+                    crc32 = (uint)BitConverter.ToInt32(c, 0)
                 };
-                fheader.Add(h);
+
+                chunk.fheader.Add(h);
+
+                chunkFilesProcessed++;
+
+                if (chunkFilesProcessed != 255) //Chunks can only hold 255 assets. We create a new chunk after we hit this limit.
+                {
+                    continue;
+                }
+
+                chunk.FileCount = chunkFilesProcessed;
+                chunks.Add(chunk);
+
+                chunk = new Chunk();
+
+                chunkFilesProcessed = 0;
             }
 
-            //Create a chunk header for the pack file
-            ChunkHeader header = new ChunkHeader()
-            {
-                NextChunkOffset = 0,
-                FileCount = (uint)fileCount,
-                files = fheader.ToArray(),
-            };
+            chunk.FileCount = chunkFilesProcessed;
+            chunks.Add(chunk);
 
-            //Update the files with the offset of each data chunk of the file
-            int length = header.Encode().Length;
-            int offset = 0;
-            for (int i = 0; i < fheader.Count; i++)
+            //Create a chunk header for each chunk
+            for (int i = 0; i < chunks.Count; i++)
             {
-                if (i != 0) offset += fileData[i - 1].Length;
-                fheader[i].offset = (uint)length + (uint)offset;
+                Chunk chunki = chunks[i];
+
+                chunki.NextChunkOffset = 0;
+                chunki.files = chunki.fheader.ToArray();
+
+                //Update the files with the offset of each data chunk of the file
+                int length = chunki.Encode().Length;
+                int offset = 0;
+                if (i > 0)
+                {
+                    offset = (int)chunks[i - 1].NextChunkOffset;
+                }
+
+                for (int j = 0; j < chunki.fheader.Count; j++)
+                {
+                    if (j != 0)
+                    {
+                        offset += chunki.fileData[j - 1].Length;
+                    }
+
+                    chunki.fheader[j].offset = (uint)length + (uint)offset;
+                }
+
+                offset += chunki.fileData[chunki.fheader.Count - 1].Length;
+                offset += length;
+
+                //There is no next chunk.
+                if (i == chunks.Count - 1)
+                {
+                    break;
+                }
+
+                chunki.NextChunkOffset = (uint)offset;
             }
-            //Write the chunk to a file
+
+            //Write the chunks to a file
             using (EndianBinaryWriter wr = new EndianBinaryWriter(EndianBitConverter.Big, File.Open(savePath, FileMode.OpenOrCreate)))
             {
-                byte[] ph = header.Encode();
-                wr.Write(ph);
-                for (int i = 0; i < fileData.Count; i++)
+                foreach (Chunk chunki in chunks)
                 {
-                    wr.Write(fileData[i]);
+                    byte[] ph = chunki.Encode();
+                    wr.Write(ph);
+
+                    foreach (byte[] t in chunki.fileData)
+                    {
+                        wr.Write(t);
+                    }
                 }
             }
-            
+
         }
     }
 }
